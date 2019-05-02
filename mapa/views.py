a5f580ab5b2856c3d5532c39b5bffba94d12bf50
unknown <Andres Felipe Sanchez Cruz>
2019-05-01 23:23:21 -0500

    Creacion de variables para guardar info de denuncia y barrios

--- mapa/views.py
+++ mapa/views.py
@@ -2,13 +2,17 @@ from django.utils.safestring import mark_safe
 from mapa.dbcontroller import Dbcontroller
 
 class Views():
+    barrios = None
+    denuncias = None
     def __init__(self):
         self.__dbcontroller = Dbcontroller()
 
-    def mapa(self, request):
-        barrios = self.__dbcontroller.obtener_datos_barrio()
-        denuncias = self.__dbcontroller.obtener_denuncias_por_barrio()
-        ctx = {"barrios": mark_safe(barrios), "denuncias": mark_safe(denuncias)}
+    def mapa(self, request):        
+        if self.barrios == None:
+            self.barrios = self.__dbcontroller.obtener_datos_barrio()        
+        if self.denuncias == None:
+            self.denuncias = self.__dbcontroller.obtener_denuncias_por_barrio()
+        ctx = {"barrios": mark_safe(self.barrios), "denuncias": mark_safe(self.denuncias)}
         return ctx
 
 #class Views():
