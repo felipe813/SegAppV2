2b4f74fe841ad904880fda8244e823406df72b7f
Andres Sanchez <felipe_813@hotmail.com>
2019-05-26 21:30:45 -0500

    filtro fecha

--- static/js/mapa.js
+++ static/js/mapa.js
@@ -1,3 +1,7 @@
+$(".dropdown-menu li").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
 var maximoIndice = 1;
 var minimoIndice = 0;
 var slider = new Slider("#ex2");
